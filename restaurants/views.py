270836c8dc5af06d3b5116f5cb4f2c950252d3ee
Hussain Al Kazemi <mhk.alk@gmail.com>
2019-09-30 18:46:33 +0300

    task02 completed

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+def greetings(request):
+	context = {
+		"msg": "Hello World!",
+
+
+	}
+	return render(request, 'task02.html', context)
 # Create your views here.
